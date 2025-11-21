bd94c75b520670c70d6feb9f9a81cf2f16c0f272
skye-pa1n <sonicgames772@gmail.com>
2025-11-21 11:57:52 -0300

    build: add my build script

---
+++ build-neutron.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+#
+# Compile script for kernel
+# Copyright (C) 2020-2024 Adithya R. and Contributors
+
+SECONDS=0 # builtin bash timer
+SUPPORTED_DEVICES=(aljeter aljeter_recovery sanders sanders_recovery)
+
+if [[ " ${SUPPORTED_DEVICES[@]} " =~ " $1 " ]]; then
+    DEVICE=$1
+    ARGUMENT=$2
+else
+    echo -e "\nSelect the device to compile:"
+    select DEVICE in "${SUPPORTED_DEVICES[@]}"; do
+        if [[ " ${SUPPORTED_DEVICES[@]} " =~ " ${DEVICE} " ]]; then
+            ARGUMENT=$1
+            break
+        else
+            echo -e "\nInvalid option. Please choose again."
+        fi
+    done
+fi
+sudo apt install binutils-arm-linux-gnueabi gcc-arm-linux-gnueabi
+
+ZIPNAME="Kernel-${DEVICE}-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$HOME/toolchains/neutron-clang"
+AK3_DIR="$(pwd)/android/AnyKernel3"
+DEFCONFIG="${DEVICE}_defconfig"
+
+if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
+   head=$(git rev-parse --verify HEAD 2>/dev/null); then
+    ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+fi
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+    echo "Neutron clang not found! Cloning to $TC_DIR..."
+    mkdir -p "$HOME/toolchains"
+    mkdir -p "$TC_DIR"
+    if ! wget -O "$HOME/toolchains/neutron-clang.tar.zst" https://github.com/Neutron-Toolchains/clang-build-catalogue/releases/download/10032024/neutron-clang-10032024.tar.zst; then
+        echo "Cloning failed! Aborting..."
+        exit 1
+    fi
+    tar -I zstd -xf $HOME/toolchains/neutron-clang.tar.zst -C "$TC_DIR"
+    rm -rf $HOME/toolchains/neutron-clang.tar.zst
+fi
+
+if [[ $ARGUMENT = "-r" || $ARGUMENT = "--regen" ]]; then
+    mkdir -p out
+    make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+    cp out/defconfig arch/arm64/configs/$DEFCONFIG
+    echo -e "\nDefconfig successfully regenerated at $DEFCONFIG"
+    exit 0
+fi
+
+if [[ $ARGUMENT = "-rf" || $ARGUMENT = "--regen-full" ]]; then
+    mkdir -p out
+    make O=out ARCH=arm64 $DEFCONFIG
+    cp out/.config arch/arm64/configs/$DEFCONFIG
+    echo -e "\nFull defconfig successfully regenerated at $DEFCONFIG"
+    exit 0
+fi
+
+if [[ $ARGUMENT = "-c" || $ARGUMENT = "--clean" ]]; then
+    rm -rf out
+fi
+
+if [[ $ARGUMENT = "-nz" || $ARGUMENT = "--no-zip" ]]; then
+    NOZIP=true
+else
+    NOZIP=false
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 \
+    CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm \
+    OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
+    CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+    LLVM=1 LLVM_IAS=1 Image.gz-dtb
+
+kernel="out/arch/arm64/boot/Image.gz"
+
+if [ -f "$kernel" ]; then
+    echo -e "\nKernel compiled successfully!"
+
+    if [ "$NOZIP" = false ]; then
+        echo -e "Preparing zip...\n"
+        if [ -d "$AK3_DIR" ]; then
+            cp -r $AK3_DIR AnyKernel3
+        elif ! git clone -q https://github.com/skye-tachyon/AnyKernel3- -b $DEVICE AnyKernel3; then
+            echo -e "\nAnyKernel3 repo not found locally and failed to clone from GitHub! Aborting..."
+            exit 1
+        fi
+        cp $kernel AnyKernel3
+        rm -rf out/arch/arm64/boot
+        cd AnyKernel3
+        git checkout $DEVICE &> /dev/null
+        zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+        cd ..
+        rm -rf AnyKernel3
+        echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!"
+        echo "Zip: $ZIPNAME"
+    else
+        echo -e "\nZip creation skipped by user."
+        echo -e "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!"
+    fi
+else
+    echo -e "\nCompilation failed!"
+    exit 1
+fi
